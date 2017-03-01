e13b2fe57e104b9e9fecf5f6081e244af0562942
FangBinwei <fangbinwei@qq.com>
2017-03-01 15:55:02 +0800

    fix bugs

--- BaiDuPicCrawler.py
+++ BaiDuPicCrawler.py
@@ -4,6 +4,7 @@ import requests
 from requests import exceptions
 import json
 import pprint
+import time
 import os
 URL = 'https://image.baidu.com/search/acjson'
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
@@ -50,59 +51,65 @@ def getPicURL(searchWord, pn):
         print e.message
     except Exception as e:
         print 'something wrong!'
+        raise Exception
     else:
-        # print response.content
         # print json.dumps(json.loads(response.content), indent=4)
-        # print '>>>>url'
-        # print response.url
         # print response.json()
         responseContent = json.loads(response.content)
         # print type(responseContent['bdFmtDispNum'])
         # print type(responseContent['bdFmtDispNum'].encode('utf8'))
-        picSum = int(filter(str.isalnum,
-                     responseContent['bdFmtDispNum'].encode('utf-8')))
-
-        if not picSum:
-            print 'no picture find'
-            exit()
-        else:
-            print 'there are about ' + str(picSum) + ' pictures'
-
-        URLList = []
-        for num in range(30):
-            try:
-                picURL = responseContent['data'][num]['replaceUrl'
-                                                      ][0]['ObjURL']
-            except KeyError as e:
-                print num
-                print e.message, '没有ObjURL'
-                picURL = responseContent['data'][num]['thumbURL']
-
-            # print type(picURL)
-            # print type(picURL.encode('utf-8'))
-            URLList.append(picURL.encode('utf-8'))
-
-        pprint.pprint(URLList)
-        basePath = os.getcwd()+'/'+searchWord+'/'
-        picIndex = len(os.listdir(basePath))+1
-        for num in range(len(URLList)):
-            postFix = URLList[num].split('.')[-1]
-            fileName = basePath+searchWord+str(picIndex)+'.'+postFix
-
-            downloadPic(URLList[num], fileName=fileName)
-            picIndex += 1
+        URLList = parseResponse(responseContent)
+        return URLList
 
 
-def downloadPic(URL, fileName):
-    try:
-        response = requests.get(URL, headers=headers, stream=True)
-        response.raise_for_status()
-        with open(fileName, 'wb') as f:
-            print 'download ', fileName
-            for chunk in response:
-                f.write(chunk)
-    except exceptions.HTTPError as e:
-        print e.message
+def parseResponse(responseContent):
+    picSum = int(filter(str.isalnum,
+                        responseContent['bdFmtDispNum'].encode('utf-8')))
+
+    if not picSum:
+        print 'no picture find'
+        exit()
+    else:
+        print 'there are about ' + str(picSum) + ' pictures'
+
+    URLList = []
+    for num in range(30):
+        try:
+            picURL = responseContent['data'][num
+                                             ]['replaceUrl'][0]['ObjURL']
+        except KeyError as e:
+            print num
+            print e.message, '没有ObjURL'
+            picURL = responseContent['data'][num]['thumbURL']
+
+        # print type(picURL)
+        # print type(picURL.encode('utf-8'))
+        URLList.append(picURL.encode('utf-8'))
+    return URLList
+
+
+def downloadPic(searchWord, pn):
+    URLList = getPicURL(searchWord, pn)
+    # print len(URLList)
+    # pprint.pprint(URLList)
+    basePath = os.getcwd() + '/' + searchWord + '/'
+    picIndex = len(os.listdir(basePath)) + 1
+    for num in range(len(URLList)):
+        postFix = URLList[num].split('.')[-1]
+        fileName = basePath + searchWord + str(picIndex) + '.' + postFix
+
+        try:
+            response = requests.get(URLList[num], headers=headers, stream=True)
+            time.sleep(1)
+            response.raise_for_status()
+            with open(fileName, 'wb') as f:
+                print 'download ', fileName
+                for chunk in response:
+                    f.write(chunk)
+        except exceptions.HTTPError as e:
+            print e.message
+
+        picIndex += 1
 
 
 def startCrawler(searchWord, startPage=1, endPage=1):
@@ -116,9 +123,13 @@ def startCrawler(searchWord, startPage=1, endPage=1):
 
     for page in range(startPage, endPage + 1):
         startPicIndex = (page - 1) * 30
-        getPicURL(searchWord, startPicIndex)
+        try:
+            downloadPic(searchWord, startPicIndex)
+            time.sleep(5)
+        except:
+            print 'start download next page'
 
 
 if __name__ == '__main__':
 
-    startCrawler('花名未闻', startPage=5, endPage=5)
+    startCrawler('angel beats', startPage=3, endPage=4)
